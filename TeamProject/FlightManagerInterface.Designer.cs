d7b52cd2339b92961f0a2d6b5ff2d793f3c46b92
nikola <nikolaet_23@abv.bg>
2022-01-11 23:35:00 +0200

    Fix

--- TeamProject/FlightManagerInterface.Designer.cs
+++ TeamProject/FlightManagerInterface.Designer.cs
@@ -1,6 +1,7 @@
-﻿namespace TeamProject
+﻿
+namespace TeamProject
 {
-    partial class Form1
+    partial class FlightManagerInterface
     {
         /// <summary>
         /// Required designer variable.
@@ -31,7 +32,7 @@
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Text = "FlightManagerInterface";
         }
 
         #endregion
--- TeamProject/Program.cs
+++ TeamProject/Program.cs
@@ -1,22 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 using System.Windows.Forms;
 
 namespace TeamProject
 {
     static class Program
     {
+        static FlightManagerContext db;
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FlightManagerInterface());
+
+            using (db = new FlightManagerContext())
+            {
+                while (true)
+                {
+                    ListOfCommands();
+                }
+            }
+        }
+        static void ListOfCommands()
+        {
+            Console.WriteLine("Type '1' and you will see the Reservation commands");
+            Console.WriteLine("Type '2' and you will see the User commands");
+            Console.WriteLine("Type '3' and you will see the Flight commands");
+            Console.WriteLine("Type '0' and you will Exit the program");
+            Console.WriteLine("Press 'Enter' and you will return to menu");
+            Console.WriteLine();
+            Console.Write("You can enter your command: ");
+            string input = Console.ReadLine();
+
+            //int selection = -1;
+            //if (int.TryParse(input, out selection))
+            //{
+            //    if (selection == 0)
+            //    {
+            //        Environment.Exit(0);
+            //    }
+            //    else if (selection == 1)
+            //    {
+            //        ReservationMenu.ListOfStoreCommands(db);
+            //    }
+            //    else if (selection == 2)
+            //    {
+            //        ReservationMenu.ListOfProductCommands(db);
+            //    }
+            //    else if (selection == 3)
+            //    {
+            //       ReservationMenu.ListOfLocationCommands(db);
+            //    }
+            //}
         }
     }
 }
