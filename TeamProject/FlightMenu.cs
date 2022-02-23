88e4944893c735bc78cd0400729abc85d7ac68bc
Plamen Lyubenov <plamen_p_l@abv.bg>
2022-02-23 17:33:08 +0200

    FlightMenu

--- TeamProject/FlightMenu.cs
+++ TeamProject/FlightMenu.cs
@@ -8,273 +8,300 @@ namespace TeamProject
 {
     class FlightMenu
     {
-        public static void ListOfFlightCommands(FlightManagmentContext db)
-
-    }
-    Console.WriteLine();
-        Console.WriteLine("Type '1' and you will Create the Flight");
-        Console.WriteLine("Type '2' and you will Read the Flight");
-        Console.WriteLine("Type '3' and you will Update the Flight");
-        Console.WriteLine("Type '4' and you will Delete the Flight");
-        Console.WriteLine("Type '5' and you will add Tickets to the Flight");
-        Console.WriteLine("Type '0' and you will exit the program");
-        Console.WriteLine("Press 'Enter' and you will return to menu");
-        Console.WriteLine();
-        Console.WriteLine("You can enter your command:");
-        string input = Console.ReadLine();
-
-    int selection = -1;
-        if (int.TryParse(input, out selection))
+        public static void ListOfFlightCommands(FlightManagementContext db)
         {
-            if (slection == 0)
-        {
-            Enviroment.Exit(0);
-        }
-            else if (selection == 1)
-{
-    FlightCreate(db);
+            Console.WriteLine();
+            Console.WriteLine("Type '1' and you will Create the Flight");
+            Console.WriteLine("Type '2' and you will Read the Flight");
+            Console.WriteLine("Type '3' and you will Update the Flight");
+            Console.WriteLine("Type '4' and you will Delete the Flight");
+            Console.WriteLine("Type '5' and you will add Ticket to the Flight");
+            Console.WriteLine("Type '0' and you will Exit the program");
+            Console.WriteLine("Press 'Enter' and you will return to menu");
+            Console.WriteLine();
+            Console.Write("You can enter your command: ");
+            string input = Console.ReadLine();
 
-}
-else if (selection == 2)
-{
-    Flight Read(db);
-}
-else if (selection == 3)
-{
-    Flight Update(db);
-}
-else if (selection == 4)
-{
-    Flight Delete(db);
-}
-else if (selection == 5)
-{
-    AddUserToFlight(db);
-
-}
+            int selection = -1;
+            if (int.TryParse(input, out selection))
+            {
+                if (selection == 0)
+                {
+                    Environment.Exit(0);
+                }
+                else if (selection == 1)
+                {
+                    FlightCreate(db);
+                }
+                else if (selection == 2)
+                {
+                    FlightRead(db);
+                }
+                else if (selection == 3)
+                {
+                    FlightUpdate(db);
+                }
+                else if (selection == 4)
+                {
+                    FlightDelete(db);
+                }
+                else if (selection == 5)
+                {
+                    AddUserToFlight(db);
+                }
             }
         }
-}
-static void FlightCreate(FlightManagmentContext db)
-{
-    Console.Write("Flight Location: ");
-    string adr = Console.ReadLine();
-
-    Console.Write("Tickets Restock Day; ");
-    int rtd = int.Parse(Console.ReadLine());
-
-    if (adr == "" || rtd == 0)
-    {
-        Console.WriteLine("Canceled!");
-    }
-    else
-    {
-        Reservation reservation = PickReaservation(db);
-        if (reservation == null)
+        static void FlightCreate(FlightManagementContext db)
         {
-            Console.WriteLine("Canceled!");
-        }
-        else
-        {
-            Flight flight = new Flight(adr, rtd, flight);
-            db.Flight.Add(flight);
-            db.SaveChanges();
-            Console.WriteLine("Flight has been added!");
-
-        }
-    }
-}
-static void (FlightManagmentContext db)
-{
-    Console.WriteLine("\n Flights: ");
-
-    foreach (var flight in DBNull.Flight)
-    {
-        Console.WriteLine(flight);
-
-    }
-    Console.WriteLine();
-}
-static void FlightUpdate(FlightManagmentContext db)
-{
-    FlightRead(db);
-
-    Console.Write("Select Flight: ");
-    string input = Console.ReadLine();
-    int selection = -1;
-    if (int.TryParse(input, out selection))
-    {
-        Flight item = db.Flights.FirstOrDefault(location => FlightCreate().Id == selection);
-
-        if (item == null)
-        {
-            Console.Write("Flight was not Found!");
-        }
-        else
-        {
-            Console.Write("Enter new Flight location: ");
+            Console.Write("Flight Location: ");
             string adr = Console.ReadLine();
 
-            Console.Write("Enter new ticket restock day: ");
+            Console.Write("Ticket Restock Day: ");
             int rtd = int.Parse(Console.ReadLine());
 
-            Console.Write("Enter new Reservation: ");
-            Ticket ticket = PickTicket(db);
-
-            if (reservation == null || adr == "" || rtd == 0)
+            if (adr == "" || rtd == 0)
             {
-                Console.Write("Canceled!");
-                return;
+                Console.WriteLine("Canceled!");
             }
             else
             {
-                item.Address = adr;
-                item.RestockDay = rtd;
-                item.Resrvation = ticket;
-                db.SaveChanges();
-                Console.Write("The Flight has been updated!");
-
-
-            }
-
-        }
-    }
-}
-
-static void FlightDelete(FlightManagmentCOntext db)
-{
-    FlightRead(db);
-
-    Console.Wrtie("Select FLight: ");
-    string input = Console.ReadLine();
-
-    int selection = -1;
-    if (int.TryParse(input, out selection))
-    {
-        Fligth item = db.Flight.FirstOrDefault(flight => flight.Id == flight);
-
-        if (item == null)
-        {
-            Console.Write("Flight was not found! ");
-
-        }
-        else
-        {
-            db.Flight.Remove(item);
-            db.SaveChanges();
-            Console.Write("Flight has been removed!");
-        }
-    }
-}
-
-static Resrvation PickReservation(FlightManagmentCOntext db)
-{
-    if (db.FlightCount() < 1)
-    {
-        Console.WriteLine("No Flights!");
-        return null;
-    }
-
-    Console.WriteLine("No Flights!");
-    return null;
-}
-Console.WriteLine("Flight: ");
-foreach (var flight in db.Flight)
-{
-    Console.WriteLine(flight);
-
-}
-
-Console.Write("Select Flight to Location: ");
-string input = Console.ReadLine();
-
-while (true)
-{
-    int selection = -1;
-    if (int.TryParse(input, out slection))
-    {
-        FlightCreate item = DBNull.Flight.FirstOrDefault(flight => flight.Id == selection);
-
-        if (item != null)
-        {
-            return item;
-        }
-    }
-
-   
-}
-static Flight PickFlight(FlightManagmentContext db)
-{
-    if (db.Locations.Count() < 1)
-    {
-        Console.WriteLine("No FLight!");
-        return null;
-
-    }
-    Console.WriteLine("Flight: ");
-    foreach (var flight in db.Flight)
-    {
-        Console.WriteLine(flight);
-    }
-
-    Console.Write("Select Flight to add Reaservation: ");
-    string input = Console.ReadLine();
-    while (true)
-    {
-        int selection = -1;
-        if (int.TryParse(input, out selection))
-        {
-            User item = db.User.FirstOrDefault(user.Id == selection);
-            if (item != null)
-            {
-                return item;
-            }
-        }
-        Console.WrtieLine("Ivalid Flight! Try again.");
-        return null;
-
-    }
-
-}
-static void AddFlightsToLocation(FlightManagmentContext db)
-{
-    Flight flight = PickFlight(db);
-
-    while (true)
-    {
-        Console.WriteLine("Type '1' to add Flight to Resrvation");
-        Console.WriteLine("Type '2' to remove Flight from Reaservation");
-        Console.WriteLine();
-        Console.Write("Enter your command:");
-        string input = Console.ReadLine();
-
-        int selection = -1;
-        if (int.TryParse(input, out selection))
-        {
-            if (selection = 0) ;
-            {
-                break;
-            }
-            elese if (selection == 1)
-            {
-                User user = PickUser(db);
-                if (!flight.User.Contains(user))
+                Reservation reservation = PickReservation(db);
+                if (reservation == null)
                 {
-                    flight.User.Add(user);
-                    db.SaveChanges();
+                    Console.WriteLine("Canceled!");
                 }
-            }
-            else if (selection == 2)
-            {
-                User user = PickUser(db);
-                if (flight.User.Contains(user))
+                else
                 {
-                    flight.User.Remove(user);
+                    Flight flight = new Flight(adr, rtd, flight);
+                    db.Flight.Add(flight);
                     db.SaveChanges();
+                    Console.WriteLine("Flight has been added!");
                 }
             }
         }
+        static void FlightnRead(FlightManagementContext db)
+        {
+
+            Console.WriteLine("\n Flights: ");
+
+            foreach (var loflight in db.Flight)
+            {
+                Console.WriteLine(flight);
+            }
+            Console.WriteLine();
+        }
+
+        static void FlightUpdate(FlightManagementContext db)
+        {
+            FlightRead(db);
+
+            Console.Write("Select  Flight: ");
+            string input = Console.ReadLine();
+
+            int selection = -1;
+            if (int.TryParse(input, out selection))
+            {
+
+                Flight item = db.Flights.FirstOrDefault(location => flight.Id == selection);
+
+
+                if (item == null)
+                {
+                    Console.Write(" Flight was not Found!");
+                }
+
+                else
+                {
+                    Console.Write("Enter new Flight location: ");
+                    string adr = Console.ReadLine();
+
+                    Console.Write("Enter new Ticket Restock Day: ");
+                    int rtd = int.Parse(Console.ReadLine());
+
+                    Console.Write("Enter new Reservation: ");
+                    Flight flight = PickFlight(db);
+
+                    if (reservation == null || adr == "" || rtd == 0)
+                    {
+                        Console.Write("Canceled!");
+                        return;
+                    }
+                    else
+                    {
+                        item.Address = adr;
+                        item.RestockDay = rtd;
+                        item.Reservation = store;
+                        db.SaveChanges();
+                        Console.Write("The Flight has been updated!");
+                    }
+                }
+            }
+        }
+
+        static void FlightnDelete(FlightManagementContext db)
+        {
+            FlightnRead(db);
+
+            Console.Write("Select Flight: ");
+            string input = Console.ReadLine();
+
+            int selection = -1;
+            if (int.TryParse(input, out selection))
+            {
+                Flight item = db.Flight.FirstOrDefault(lflight => flightn.Id == flight);
+
+                if (item == null)
+                {
+                    Console.Write("Flight was not Found!");
+                }
+                else
+                {
+                    db.Flights.Remove(item);
+                    db.SaveChanges();
+                    Console.Write("Flight has been removed!");
+                }
+            }
+        }
+
+        static Reservation PickReservation(FlightManagementContext db)
+        {
+
+            if (db.Flight.Count() < 1)
+            {
+                Console.WriteLine("No Flights!");
+                return null;
+            }
+
+            Console.WriteLine("Flights:");
+            foreach (var flight in db.Flights)
+            {
+                Console.WriteLine(flight);
+            }
+
+
+            Console.Write("Select Flight to Location: ");
+            string input = Console.ReadLine();
+
+            while (true)
+            {
+                int selection = -1;
+                if (int.TryParse(input, out selection))
+                {
+                    Flight item = db.Flight.FirstOrDefault(flight => flight.Id == selection);
+
+                    if (item != null)
+                    {
+                        return item;
+                    }
+                }
+            }
+        }
+        static Flight PickFlight(FlightManagementContext db)
+        {
+            if (db.Locations.Count() < 1)
+            {
+                Console.WriteLine("No Flight!");
+                return null;
+            }
+
+            Console.WriteLine("Flight: ");
+            foreach (var flight in db.Flights)
+            {
+                Console.WriteLine(flight);
+
+            }
+
+            Console.Write("Select Flight to add to Reservation: ");
+            string input = Console.ReadLine();
+
+            while (true)
+            {
+                int selection = -1;
+                if (int.TryParse(input, out selection))
+                {
+                    Flight item = db.Flights.FirstOrDefault(flight => flight.Id == flight);
+
+                    if (item != null)
+                    {
+                        return item;
+                    }
+                    Console.WriteLine("Invalid Flight! Try again.");
+                }
+            }
+        }
+        static User PickUser(FlightManagementContext db)
+        {
+            if (db.Flights.Count() < 1)
+            {
+                Console.WriteLine("No Flight!");
+                return null;
+            }
+
+            Console.WriteLine("Flight:");
+            foreach (var product in db.Flights)
+            {
+                Console.WriteLine(flight);
+            }
+            Console.Write("Select  Flight to add to Reservation: ");
+            string input = Console.ReadLine();
+
+            while (true)
+            {
+                int selection = -1;
+                if (int.TryParse(input, out selection))
+                {
+                    User item = db.Users.FirstOrDefault(user => user.Id == selection);
+                    if (item != null)
+                    {
+                        return item;
+                    }
+                }
+
+                Console.WriteLine("Invalid Flight! Try again.");
+                return null;
+            }
+        }
+        static void AddFlightsToLocation(FlightManagementContext db)
+        {
+            Flight flight = PickFlight(db);
+
+            while (true)
+            {
+                Console.WriteLine("Type '1' to add Flight to Reservation");
+                Console.WriteLine("Type '2' to remove Flight from Reservation");
+                Console.WriteLine("Type '0' to return in Main menu ");
+                Console.WriteLine();
+                Console.Write("Enter your command: ");
+                string input = Console.ReadLine();
+
+                int selection = -1;
+                if (int.TryParse(input, out selection))
+                {
+                    if (selection == 0)
+                    {
+                        break;
+                    }
+                    else if (selection == 1)
+                    {
+                        User user = PickUser(db);
+                        if (!Flight.Users.Contains(user))
+                        {
+                            flight.Users.Add(user);
+                            db.SaveChanges();
+                        }
+                    }
+                    else if (selection == 2)
+                    {
+                        User user = PicUser(db);
+                        if (Flight.User.Contains(user))
+                        {
+                            Flight.Users.Remove(user);
+                            db.SaveChanges();
+                        }
+                    }
+                }
+            }
+        }
+
     }
 }
-    }
-    }
